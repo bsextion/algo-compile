0bdd30dfe28c178301f2a40615c44a5454d0978c
bsextion <bsextion@gmail.com>
2023-11-06 23:59:57 -0600

    updated logic to use SSG

--- src/pages/problems/[pid].tsx
+++ src/pages/problems/[pid].tsx
@@ -1,15 +1,47 @@
 import Topbar from '@/components/Topbar/Topbar';
 import Workspace from '@/components/Workspace/Workspace';
-import React from 'react';
+import { problems } from '.';
+import { Problem } from '@/utils/problems/types/problem';
 
-type ProblemPageProps = {};
+type ProblemPageProps = {
+problem: Problem;
+};
 
-const ProblemPage: React.FC<ProblemPageProps> = () => {
+const ProblemPage: React.FC<ProblemPageProps> = ({problem}) => {
   return (
     <div>
-      <Topbar problemPage={true} />
-      <Workspace/>
+      <Topbar problemPage />
+      <Workspace problem={problem}/>
     </div>
   );
 };
 export default ProblemPage;
+
+
+export async function getStaticPaths() {
+  const paths = Object.keys(problems).map((key) => ({
+    params: { pid: key },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+//getStaticProps => fetch data for the dynamic routes
+export async function getStaticProps({ params}: {params: {pid:string}}) {
+  const { pid } = params;
+  const problem = problems[pid];
+
+  if(!problem) {
+    return {
+      notFound: true,
+    }
+  }
+  problem.handlerFunction = problem.handlerFunction.toString();
+ return {
+  props: {
+    problem
+  }
+ }
+}
---
+++ src/pages/problems/index.ts
@@ -0,0 +1,17 @@
+import { jumpGame } from "@/utils/problems/jump-game";
+import { reverseLinkedList } from "@/utils/problems/reverse-linked-list";
+import { search2DMatrix } from "@/utils/problems/search-a-2d-matrix";
+import { twoSum } from "@/utils/problems/two-sum";
+import { Problem } from "@/utils/problems/types/problem";
+import { validParentheses } from "@/utils/problems/valid-parentheses";
+
+interface ProblemMap {
+    [key: string]:Problem
+}
+export const problems: ProblemMap = {
+    'two-sum': twoSum,
+    'reverse-linked-list': reverseLinkedList,
+    'jump-game': jumpGame,
+    'search-2d-matrix': search2DMatrix,
+    'valid-parentheses': validParentheses
+}

1e98630767a6a525c20b53b54ba28744c0e9a455
bsextion <bsextion@gmail.com>
2023-11-08 20:04:06 -0600

    topbar buttons update

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -22,15 +22,23 @@ const Topbar: React.FC<TopbarProps> = ({ problemPage }) => {
   const router = useRouter();
 
   const handleProblemChange = (isForward: boolean) => {
-    const {order} = problems[router.query.pid as string] as Problem
-    const directon = isForward ? 1 : -1;
-    const nextProblemOrder = order + directon;
-    const nextProblemKey = Object.keys(problems).find(key => problems[key].order === nextProblemOrder)
-    console.log(nextProblemKey)
-    if (isForward && !nextProblemKey) {
+		const { order } = problems[router.query.pid as string] as Problem;
+		const direction = isForward ? 1 : -1;
+		const nextProblemOrder = order + direction;
+		const nextProblemKey = Object.keys(problems).find((key) => problems[key].order === nextProblemOrder);
 
-    }
-  };
+		if (isForward && !nextProblemKey) {
+			const firstProblemKey = Object.keys(problems).find((key) => problems[key].order === 1);
+			router.push(`/problems/${firstProblemKey}`);
+		} else if (!isForward && !nextProblemKey) {
+			const lastProblemKey = Object.keys(problems).find(
+				(key) => problems[key].order === Object.keys(problems).length
+			);
+			router.push(`/problems/${lastProblemKey}`);
+		} else {
+			router.push(`/problems/${nextProblemKey}`);
+		}
+	};
   return (
     <nav className='relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7'>
       <div
